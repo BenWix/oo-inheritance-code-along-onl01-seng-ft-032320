309fa519231c707ffeea3a6b76a455d514eeea40
BenWix <benwichser@gmail.com>
2020-04-02 15:36:31 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle #this is how we inherit from another class
+   
+    def go 
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end 
+end 
+
+#the class will ingerit everyhting from the parent class, but we can overwrite whatever we want
